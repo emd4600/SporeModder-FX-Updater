3f09ed85dc1d0bd5ef47341b10f2e28cee0ee3c3
Eric Mor <emd4600@gmail.com>
2023-12-06 21:36:08 +0100

    Update collect_reg_changes.sh

--- collect_reg_changes.sh
+++ collect_reg_changes.sh
@@ -1,3 +1,3 @@
-cd SporeModder-FX
+# cd SporeModder-FX
 lastTagCommit=$(git rev-list -n 1 $(git describe --tags --abbrev=0))
 git diff -U0 $lastTagCommit $1 | grep '^[+]' | grep -Ev '^(--- a/|\+\+\+ b/)' | cut -c2- 
